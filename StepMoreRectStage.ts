c674b884001e3fc06cb9c744cabebe81dda2a41f
Anwesh43 <anweshthecool0@gmail.com>
2018-10-14 14:44:03 +0530

    adding SMRNode class

--- StepMoreRectStage.ts
+++ StepMoreRectStage.ts
@@ -64,3 +64,58 @@ class Animator {
         }
     }
 }
+
+class SMRNode {
+    state : State = new State()
+    next : SMRNode
+    prev : SMRNode
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < nodes - 1) {
+            this.next = new SMRNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        const gap : number = w / (nodes + 1)
+        const size_factors = [3, 4]
+        const colors = ["#4527A0", "white"]
+        context.save()
+        context.translate(gap * this.i + gap, h/2)
+        for (var j = 0; j < 2; j++) {
+            const sc : number = Math.min(0.5, Math.max(0, this.state.scale - 0.5 * j)) * 2
+            context.fillStyle = colors[j]
+            context.save()
+            context.scale(sc, sc)
+            const size = gap / size_factors[j]
+            context.fillRect(-size, -size, 2 * size, 2 * size)
+            context.restore()
+        }
+        context.restore()
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : SMRNode {
+        var curr : SMRNode = this.prev
+        if (dir == 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
